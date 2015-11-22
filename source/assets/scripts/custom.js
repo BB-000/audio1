80c7f92a34f23f982918b2570ce2aecad1802c9e
BB-000 <spambobular@live.co.uk>
2015-11-22 15:05:37 +0000

    content grid v1

--- source/assets/scripts/custom.js
+++ source/assets/scripts/custom.js
@@ -6,3 +6,95 @@
 	    $(this).html(string);
 	});
 }
+
+
+$(function() {
+
+// $( ".openMore" ).click().next().toggle();
+
+// $(document).on("click", ".openMore", function(event){
+//     event.preventDefault();
+//     var nexxt = $(this).next('.show-more');
+//     nexxt.fadeToggle("slow");
+//     console.log("ff");
+
+    // var arow = $(this).next('.show-arrow');
+    // arow.toggleClass('.rotate')
+
+    // $('.show-arrow').toggleClass('.rotate');
+
+
+$("li.openMore").click(function(){
+    $(this).parent().find('.show-more').fadeToggle();
+    $(this).parent().find('.show-arrow').toggleClass('rotate');
+});
+
+// $(".openMore").click(function(){
+//     $(this).parent().next('.show-more').fadeToggle();
+// });
+
+// $('.openMore').click(function(){
+// 		$('.open-more').find('.show-arrow').toggleClass('.rotate');
+// } );
+// 
+
+  // $('.show-arrow').click(function() {
+  //   	$( this ).toggleClass( "rotate" );
+  //   })
+
+}); 
+
+
+
+// $('.show-arrow').click(function(){
+//     $(this).rotate(90);
+//     });
+// });
+
+// $('.openMore').on("click", function (event) {
+//     $('.show-arrow').toggleClass('.rotate');
+//     // $('.show-arrow').toggleClass('.rotate-reset');
+// });
+
+
+
+// });
+
+
+
+// $('.grid').masonry({
+//   // options...
+//   itemSelector: '.grid-item',
+//   columnWidth: 200
+// });
+
+
+// // or with jQuery
+// var $grid;
+
+// function triggerMasonry() {
+//   // don't proceed if $grid has not been selected
+//   if ( !$grid ) {
+//     return;
+//   }
+//   // init Masonry
+//   $grid.masonry({
+//     // options...
+//       itemSelector: '.grid-item',
+//   columnWidth: 200
+//   });
+// }
+// // trigger masonry on document ready
+// $(function(){
+//   $grid = $('.grid');
+//   triggerMasonry();
+// });
+// // trigger masonry when fonts have loaded
+// Typekit.load({
+//   active: triggerMasonry,
+//   inactive: triggerMasonry
+// });
+
+
+// $('.show-arrow').click()
+
